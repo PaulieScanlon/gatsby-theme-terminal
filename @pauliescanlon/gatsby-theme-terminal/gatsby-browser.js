913ba3a1a72c53cf124fd2d00f6c50d307a1d33c
pauliescanlon <pauliescanlon@gmail.com>
2020-02-24 10:49:27 +0530

    add anchor scroll

--- @pauliescanlon/gatsby-theme-terminal/gatsby-browser.js
+++ @pauliescanlon/gatsby-theme-terminal/gatsby-browser.js
@@ -1,7 +1,29 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+const anchorScroll = location => {
+  const anchor = document.querySelectorAll(`a[href="${location.hash}"]`)[0]
+  if (location && location.hash && anchor) {
+    const item = document.querySelectorAll(`a[href="${location.hash}"]`)[0]
+      .offsetTop
+    const mainNavHeight = document.querySelector(`header`).offsetHeight
 
-// You can delete this file if you're not using it
+    setTimeout(() => {
+      window.scrollTo({
+        top: item - mainNavHeight,
+        behavior: 'smooth',
+      })
+    }, 50)
+  }
+}
+
+exports.shouldUpdateScroll = ({ routerProps: { location } }) => {
+  if (location && location.action === 'PUSH') {
+    setTimeout(() => window.scrollTo({ top: 0 }), 250)
+  }
+
+  anchorScroll(location)
+  return false
+}
+
+exports.onRouteUpdate = ({ location }) => {
+  anchorScroll(location)
+  return true
+}
