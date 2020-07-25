6cef647debd55bd2546641244e4b770546db4e20
pauliescanlon <pauliescanlon@gmail.com>
2020-07-25 21:01:55 +0100

    add featured test

---
+++ demo/src/posts/2020/07/SomeComponent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const SomeComponent = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      viewBox="0 0 2048 1152"
+      width="100%"
+      height="auto"
+    >
+      <rect fill="#F7F7F7" width="100%" height="100%" />
+      <g>
+        <circle fill="#FF5CA3" cx="1323.7" cy="721.3" r="393.1" />
+        <circle fill="#00C4FF" cx="557.5" cy="276.3" r="226.3" />
+        <circle fill="#36E5D0" cx="843.2" cy="121.8" r="84.3" />
+        <circle fill="#F1C40F" cx="891.1" cy="675" r="226.3" />
+      </g>
+    </svg>
+  )
+}
