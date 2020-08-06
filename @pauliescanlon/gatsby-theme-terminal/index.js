81e2c42d36e99f4c45b1af874b9d4c87071ab156
Paul Scanlon <paul.scanlon@sage.com>
2020-08-06 16:02:01 +0100

    release 0.0.38

--- @pauliescanlon/gatsby-theme-terminal/index.js
+++ @pauliescanlon/gatsby-theme-terminal/index.js
@@ -1,6 +1,11 @@
-// export { SourceDates } from './src/components/SourceDates'
+// Data Componets
 export { SourceDays } from './src/components/SourceDays'
 export { SourceList } from './src/components/SourceList'
 export { SourceMonths } from './src/components/SourceMonths'
 export { SourceTags } from './src/components/SourceTags'
 export { SourceWords } from './src/components/SourceWords'
+
+// Data
+export { useConig } from './src/data/useConfig'
+export { useNavigation } from './src/data/useNavigation'
+export { useAllMdx } from './src/data/useAllMdx'
