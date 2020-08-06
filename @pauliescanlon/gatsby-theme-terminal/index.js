8946cf8588a00cabb6d9bedc93630164c0e60118
Paul Scanlon <paul.scanlon@sage.com>
2020-08-06 16:05:29 +0100

    v0.0.39

--- @pauliescanlon/gatsby-theme-terminal/index.js
+++ @pauliescanlon/gatsby-theme-terminal/index.js
@@ -6,6 +6,6 @@ export { SourceTags } from './src/components/SourceTags'
 export { SourceWords } from './src/components/SourceWords'
 
 // Data
-export { useConig } from './src/data/useConfig'
+export { useConfig } from './src/data/useConfig'
 export { useNavigation } from './src/data/useNavigation'
 export { useAllMdx } from './src/data/useAllMdx'
