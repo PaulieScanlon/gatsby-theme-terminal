bb8e7f411174176f470367ebc41f12de7ef2639c
Paul Scanlon <paul.scanlon@sage.com>
2020-08-06 15:01:44 +0100

    add site url to config

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
     description: `A zero component Gatsby theme for developers`,
     keywords: [`gatsby`, `gatsbyjs`, `gatsby-theme`],
     siteUrl: `https://gatsby-theme-terminal.netlify.com`,
-    siteImage: `images/terminal-open-graph-image.jpg`,
-    profileImage: `images/terminal-profile-image.jpg`,
+    siteImage: `https://gatsby-theme-terminal.netlify.com/images/terminal-open-graph-image.jpg`,
+    profileImage: `https://gatsby-theme-terminal.netlify.com/images/terminal-profile-image.jpg`,
     lang: `en`,
     config: {
       sidebarWidth: 280,
@@ -19,9 +19,9 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'UA-76055934-8',
+        trackingId: "UA-76055934-8",
       },
     },
   ],
