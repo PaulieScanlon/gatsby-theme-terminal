f6d95dd248af03c5649d49d141838298c49ab48d
pauliescanlon <pauliescanlon@gmail.com>
2021-02-15 21:51:29 +0000

    add siteUrl to font path

--- @pauliescanlon/gatsby-theme-terminal/src/components/PageElement/PageElement.js
+++ @pauliescanlon/gatsby-theme-terminal/src/components/PageElement/PageElement.js
@@ -2,24 +2,34 @@ import React from 'react'
 import { Global, css } from '@emotion/core'
 import { Fragment } from 'react'
 
-export const PageElement = ({ children }) => (
-  <Fragment>
-    <Global
-      styles={css`
+import { useConfig } from '../../data'
+
+export const PageElement = ({ children }) => {
+  const {
+    site: {
+      siteMetadata: { siteUrl },
+    },
+  } = useConfig()
+
+  return (
+    <Fragment>
+      <Global
+        styles={css`
         @font-face {
-          font-family: 'inconsolata';
-          src: url('./fonts/Inconsolata-Regular.woff') format('woff');
+          font-family: 'Inconsolata';
+          src: url('${siteUrl}/fonts/Inconsolata-Regular.woff') format('woff');
           font-weight: 400;
           font-style: normal;
         }
         @font-face {
-          font-family: 'inconsolata';
-          src: url('./fonts/Inconsolata-Bold.woff') format('woff');
+          font-family: 'Inconsolata';
+          src: url('${siteUrl}/fonts/Inconsolata-Bold.woff') format('woff');
           font-weight: 700;
           font-style: normal;
         }
       `}
-    />
-    {children}
-  </Fragment>
-)
+      />
+      {children}
+    </Fragment>
+  )
+}
